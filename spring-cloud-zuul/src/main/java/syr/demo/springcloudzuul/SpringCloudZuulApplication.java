f966e6ad7145e36a813cb94e56d018445db458ad
shangyouren <17865815615@163.com>
2019-02-10 20:21:37 +0800

    first commit with zuul

---
+++ spring-cloud-zuul/src/main/java/syr/demo/springcloudzuul/SpringCloudZuulApplication.java
@@ -0,0 +1,17 @@
+package syr.demo.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
+
+}
+
---
+++ spring-cloud-zuul/src/test/java/syr/demo/springcloudzuul/SpringCloudZuulApplicationTests.java
@@ -0,0 +1,17 @@
+package syr.demo.springcloudzuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudZuulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
